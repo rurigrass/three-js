025dd2eaf33222a5ed5ec2b5bc3d8c3850bb7efa
rurigrass <ruripg@gmail.com>
2024-01-12 00:01:09 +0000

    added controls to animation

--- 46-load-models-with-r3f/src/Fox.jsx
+++ 46-load-models-with-r3f/src/Fox.jsx
@@ -6,19 +6,30 @@ export default function Fox() {
   //NEW WAY TO LOAD WITH DREI - even supports draco whatever that is.
   const FoxModel = useGLTF("./Fox/glTF/Fox.gltf");
   const animations = useAnimations(FoxModel.animations, FoxModel.scene);
+  console.log(animations);
+
+  //Animation order on open:
+  //   useEffect(() => {
+  //     const walk = animations.actions.Walk;
+  //     walk.play();
+  //     const run = animations.actions.Run;
+  //     console.log("hey");
+  //     setTimeout(() => {
+  //       run.play();
+  //       run.crossFadeFrom(walk, 1);
+  //     }, 3000);
+  //   }, []);
+
+  //ANIMATION CONTROLS
+  const { animationName } = useControls({
+    // animationName: { options: ["Survey", "Walk", "Run"] },
+    animationName: { options: animations.names },
+  });
 
   useEffect(() => {
-    // const action = animations.actions.Run;
-    // const action = animations.actions.Survey;
-    const walk = animations.actions.Walk;
-    walk.play();
-    const run = animations.actions.Run;
-    console.log("hey");
-    setTimeout(() => {
-      run.play();
-      run.crossFadeFrom(walk, 1);
-    }, 3000);
-  }, []);
+    const action = animations.actions[animationName];
+    action.play();
+  }, [animationName]);
 
   return (
     <primitive
