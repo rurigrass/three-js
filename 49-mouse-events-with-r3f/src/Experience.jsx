950a9d60a8430049526940f2a8e4d6ad7c3fddf2
rurigrass <ruripg@gmail.com>
2024-01-15 21:30:39 +0100

    lesson 49 learnt different mousekeys js

--- 49-mouse-events-with-r3f/src/Experience.jsx
+++ 49-mouse-events-with-r3f/src/Experience.jsx
@@ -1,37 +1,69 @@
-import { useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import { useRef } from 'react'
+import { useFrame } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { useRef, useState } from "react";
 
-export default function Experience()
-{
-    const cube = useRef()
-    
-    useFrame((state, delta) =>
-    {
-        cube.current.rotation.y += delta * 0.2
-    })
+export default function Experience() {
+  const cube = useRef();
 
-    return <>
+  useFrame((state, delta) => {
+    cube.current.rotation.y += delta * 0.2;
+  });
 
-        <OrbitControls makeDefault />
+  //   const [color, setColor] = useState("orange");
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
+  const changeColor = (event) => {
+    console.log("COLOR CHANGE!", event);
+    // color === "orange" ? setColor("blue") : setColor("orange");
+    cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 66%)`);
+  };
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+  return (
+    <>
+      <OrbitControls makeDefault />
 
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+      <directionalLight position={[1, 2, 3]} intensity={4.5} />
+      <ambientLight intensity={1.5} />
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+      <mesh position-x={-2}>
+        <sphereGeometry />
+        <meshStandardMaterial color={"orange"} />
+      </mesh>
 
+      <mesh
+        ref={cube}
+        //EVENT below is onclick function
+        //   onClick={changeColor}
+        //EVENT below is on left click function
+        // onContextMenu={changeColor}
+        //EVENT below is ondoubleclick
+        // onDoubleClick={changeColor}
+        //EVENT below is on release of click
+        // onPointerUp={changeColor}
+        //EVENT below is on down click change
+        // onPointerDown={changeColor}
+        //EVENT below onPointerOver
+        // onPointerOver={changeColor}
+        //EVENT below does the same as the previous one
+        // onPointerEnter={changeColor}
+        //EVENT below do on leave/out
+        // onPointerOut={changeColor}
+        // onPointerLeave={changeColor}
+        //EVENT onPointerMove is triggered on each frame
+        // onPointerMove={changeColor}
+        //EVENT below onPointerMiss - calls it if you point outside of the cube - useful for unselecting
+        // onPointerMissed={changeColor}
+
+        position-x={2}
+        scale={1.5}
+      >
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
     </>
+  );
 }
