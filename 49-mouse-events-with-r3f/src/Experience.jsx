62e9041be2cd5e2a351bb0d155d56018d9353302
rurigrass <ruripg@gmail.com>
2024-01-17 20:17:39 +0100

    completed lesson 49

--- 49-mouse-events-with-r3f/src/Experience.jsx
+++ 49-mouse-events-with-r3f/src/Experience.jsx
@@ -1,9 +1,15 @@
-import { useFrame } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import {
+  useGLTF,
+  OrbitControls,
+  useCursor,
+  meshBounds,
+} from "@react-three/drei";
 import { useRef, useState } from "react";
 
 export default function Experience() {
   const cube = useRef();
+  const hamburger = useGLTF("/hamburger.glb");
 
   useFrame((state, delta) => {
     cube.current.rotation.y += delta * 0.2;
@@ -12,10 +18,14 @@ export default function Experience() {
   //   const [color, setColor] = useState("orange");
 
   const changeColor = (event) => {
-    console.log("COLOR CHANGE!", event);
+    // console.log("COLOR CHANGE!", event);
     // color === "orange" ? setColor("blue") : setColor("orange");
     cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 66%)`);
   };
+
+  //   const { gl } = useThree();
+  const [hovered, setHovered] = useState();
+  useCursor(hovered);
 
   return (
     <>
@@ -29,6 +39,7 @@ export default function Experience() {
         onClick={(event) => {
           //THIS BELOW will stop the user from clicking anything behind this object.
           event.stopPropagation();
+          //   setHovered(false);
         }}
       >
         <sphereGeometry />
@@ -37,8 +48,12 @@ export default function Experience() {
 
       <mesh
         ref={cube}
+        //THIS BELOW CREATES AN INVISIBLE SPHERE ON THE OBJECT TO CLICK ON - !!INCREASES PERFORMANCE!! BUT CAN ONLY WORK ON MESHES.
+        // raycast={meshBounds}
+        position-x={2}
+        scale={1.5}
         //EVENT below is onclick function
-        // onClick={changeColor}
+        onClick={changeColor}
         //EVENT below is on left click function
         // onContextMenu={changeColor}
         //EVENT below is ondoubleclick
@@ -57,9 +72,17 @@ export default function Experience() {
         //EVENT onPointerMove is triggered on each frame
         // onPointerMove={changeColor}
         //EVENT below onPointerMiss - calls it if you point outside of the cube - useful for unselecting
-        onPointerMissed={changeColor}
-        position-x={2}
-        scale={1.5}
+        // onPointerMissed={changeColor}
+        onPointerEnter={() => {
+          //   document.body.style.cursor = "pointer";
+          //   console.log("POINTER ", gl.domElement.attributeStyleMap.set()); -- theeres a way here to get the canvas style instead of the body but its hard to figure out
+          setHovered(true);
+        }}
+        onPointerLeave={() => {
+          //BELOW IS THE NATIVE JS WAY
+          //   document.body.style.cursor = "default";
+          setHovered(false);
+        }}
       >
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
@@ -69,6 +92,18 @@ export default function Experience() {
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
+
+      <primitive
+        object={hamburger.scene}
+        scale={0.25}
+        position-y={0.5}
+        onClick={(event) => {
+          //lets you select the whole hamburger once without having to click all of the parts of it.
+          event.stopPropagation();
+
+          console.log(event.object);
+        }}
+      />
     </>
   );
 }
