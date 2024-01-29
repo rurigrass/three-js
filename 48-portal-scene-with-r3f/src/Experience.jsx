fb18b8412faa8b4132763f183285d49aa70dd6b6
rurigrass <ruripg@gmail.com>
2024-01-29 22:38:06 +0100

    added planet and ambient light

--- 48-portal-scene-with-r3f/src/Experience.jsx
+++ 48-portal-scene-with-r3f/src/Experience.jsx
@@ -19,6 +19,13 @@ export default function Experience() {
       {/* <mesh geometry={nodes.baked.geometry}>
         <meshBasicMaterial map={bakedTexture} />
       </mesh> */}
+      <>
+        <ambientLight intensity={0.2} />
+        <directionalLight intensity={3.5} position={[1, 0.5, -0.4]} />
+        <MenuButtons position={[isMobile ? 0 : 1, 0, 2]} />
+        <Planet position={[isMobile ? 0 : -1, 0, 6]} />
+      </>
+
       <RoundedBox args={[1, 0.5, 0.1]} />
       <RoundedBox args={[1, 0.5, 0.1]} />
       <RoundedBox args={[1, 0.5, 0.1]} />
