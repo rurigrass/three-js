3e6db00639d271e7bc32982a15128de98529a4a9
rurigrass <ruripg@gmail.com>
2024-01-31 14:22:56 +0100

    lesson 52 started.

--- 52-physics-with-r3f/src/Experience.jsx
+++ 52-physics-with-r3f/src/Experience.jsx
@@ -1,31 +1,54 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import { OrbitControls, Torus } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+import { BallCollider, Physics, RigidBody } from "@react-three/rapier";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+  return (
+    <>
+      <Perf position="top-left" />
 
-        <Perf position="top-left" />
+      <OrbitControls makeDefault />
 
-        <OrbitControls makeDefault />
+      <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
+      <ambientLight intensity={1.5} />
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
-
-        <mesh castShadow position={ [ - 2, 2, 0 ] }>
+      <Physics debug>
+        <RigidBody colliders="ball">
+          <mesh castShadow position={[-2, 2, 0]}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
-        </mesh>
+          </mesh>
+        </RigidBody>
 
-        <mesh castShadow position={ [ 2, 2, 0 ] }>
-            <boxGeometry />
+        {/* <RigidBody>
+          <mesh castShadow position={[2, 2, 0]}>
+            <boxGeometry args={[3, 2, 1]} />
             <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+          </mesh>
+          <mesh castShadow position={[2, 2, 3]}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+        </RigidBody> */}
 
-        <mesh receiveShadow position-y={ - 1.25 }>
-            <boxGeometry args={ [ 10, 0.5, 10 ] } />
+        <RigidBody colliders="hull">
+          <mesh
+            castShadow
+            position={[0, 1, -0.25]}
+            rotation={[Math.PI * 0.1, 0, 0]}
+          >
+            <torusGeometry args={[1, 0.5, 16, 32]} />
+            <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+        </RigidBody>
+
+        <RigidBody type="fixed">
+          <mesh receiveShadow position-y={-1.25}>
+            <boxGeometry args={[10, 0.5, 10]} />
             <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
+          </mesh>
+        </RigidBody>
+      </Physics>
     </>
+  );
 }
